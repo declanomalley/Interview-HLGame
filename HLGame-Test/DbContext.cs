a33a4571b50d1f7acaeedea8589ca6e69bf97dc5
Declan O'Malley <nalced@hotmail.co.uk>
2021-01-29 14:51:51 +0000

    game correct answer test.

---
+++ HLGame-Test/DbContext.cs
@@ -0,0 +1,39 @@
+﻿using HLGame.Interface;
+using HLGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HLGame_Test
+{
+    public class TestDBContextHiLoGuessGame : IDBContext
+    {
+        public TestDBContextHiLoGuessGame() { }
+        public TestDBContextHiLoGuessGame(INumbersBoard board)
+        {
+            Board = board;
+        }
+
+        INumbersBoard Board;
+
+        public IGame GetGame(int ID)
+        {
+            return new HiLoGuessGame(new TestDBContextHiLoGuessGame(), Board);
+        }
+
+        public IHighScore[] GetHighScores()
+        {
+            return new List<IHighScore>().ToArray();
+        }
+
+        public bool SaveGame()
+        {
+            return true;
+        }
+
+        public bool SaveHighScore()
+        {
+            return true;
+        }
+    }
+}
---
+++ HLGame-Test/HiLoGameTests.cs
@@ -0,0 +1,55 @@
+﻿using HLGame.Interface;
+using HLGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HLGame_Test
+{
+    public class HiLoGameTests
+    {
+
+
+        TestDBContextHiLoGuessGame dbContext;
+        NumberGenerator_RandomNumbers_DifferFromPrevious NumberGenerator;
+        private const int NumberOfRounds = 10;
+
+        public HiLoGameTests()
+        {
+            
+            NumberGenerator = new NumberGenerator_RandomNumbers_DifferFromPrevious();
+            var board = new HiLoNumbersBoard(NumberGenerator);
+            dbContext = new TestDBContextHiLoGuessGame(board);
+        }
+
+
+        [Fact]
+        public void HiLoNumberBoard_Current_Number_Should_Change_On_Correct_Answer()
+        {
+            bool validated = false;
+            
+            
+
+            while (!validated)
+            {
+                var board = new HiLoNumbersBoard(NumberGenerator);
+                var Game = new HiLoGuessGame(dbContext, board);
+                var previousNumber = Game.NumbersBoard.CurrentNumber();
+                // wrong guess
+                if (!Game.Guess(true))
+                {
+                    // guessing moves to the next number so you can see what the number was
+                    Assert.True(previousNumber > Game.NumbersBoard.CurrentNumber());
+
+                }
+                else
+                {
+                    validated = true;
+                }
+            }
+        }
+
+    
+    }
+}
