7a69baf833cb2300ad13fd22cb160cb26524e289
Declan O'Malley <nalced@hotmail.co.uk>
2021-01-29 14:30:06 +0000

    compile razor pages during debug.

--- HLGame/Startup.cs
+++ HLGame/Startup.cs
@@ -24,6 +24,7 @@ namespace HLGame
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +46,8 @@ namespace HLGame
             app.UseRouting();
 
             app.UseAuthorization();
+
+            
 
             app.UseEndpoints(endpoints =>
             {
