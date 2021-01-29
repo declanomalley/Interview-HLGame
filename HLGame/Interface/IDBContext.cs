73f93901f239011432508c6259d685ae6701be2a
Declan O'Malley <nalced@hotmail.co.uk>
2021-01-29 14:46:45 +0000

    define a db context.

---
+++ HLGame/Interface/IDBContext.cs
@@ -0,0 +1,18 @@
+﻿namespace HLGame.Interface
+{
+    public interface IDBContext : IDBGame, IDBHighScore
+    {
+    }
+
+    public interface IDBGame
+    {
+        public IGame GetGame(int ID);
+        public bool SaveGame();
+    }
+
+    public interface IDBHighScore
+    {
+        public IHighScore[] GetHighScores();
+        public bool SaveHighScore();
+    }
+}
