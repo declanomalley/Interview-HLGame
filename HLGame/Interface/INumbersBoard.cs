551035c336bb493c48eea67bad28ee81e78f3fb8
Declan O'Malley <nalced@hotmail.co.uk>
2021-01-29 13:38:47 +0000

    numbers board to display

---
+++ HLGame/Interface/INumbersBoard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HLGame.Interface
+{
+    public interface INumbersBoard
+    {
+        int[] Numbers { get; set; }
+        List<int> RevealedNumbers { get; set; }
+
+
+    }
+}
