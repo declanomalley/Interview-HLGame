2592cc2624a3a241818d136f6fbab04f630bb4a7
Declan O'Malley <nalced@hotmail.co.uk>
2021-01-29 14:53:03 +0000

    define guess.

---
+++ HLGame/Interface/IGuess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static HLGame.Interface.IGameState;
+
+namespace HLGame.Interface
+{
+    public interface IGuess<AnswerType>
+    {
+        public bool Guess(AnswerType Higher);
+        public bool End(eState state);
+    }
+}
