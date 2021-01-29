ec6ce376f20824d2c4f53fe633466ea6865d9e5b
Declan O'Malley <nalced@hotmail.co.uk>
2021-01-29 14:50:25 +0000

    define the game.

---
+++ HLGame/Interface/IGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static HLGame.Interface.IGameState;
+
+namespace HLGame.Interface
+{
+    public interface IGameState
+    {
+        public enum eState { Unknown = 0, Active = 1, GameOver = 2, Success = 3 }
+    }
+
+    public interface IGame
+    {
+        
+        public IUser User { get; set; }
+        public int ID { get; set; }
+        public int[] Numbers { get; set; }
+        public int Score { get; set; }
+
+        public INumbersBoard NumbersBoard { get; }
+
+        
+    }
+
+
+
+
+}
