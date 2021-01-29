f292f61928b370b7e3bb1046a95be81218f1e28e
Declan O'Malley <nalced@hotmail.co.uk>
2021-01-29 13:25:58 +0000

    generate random numbers that differ from previous.

---
+++ HLGame/Models/NumberGenerator.cs
@@ -0,0 +1,42 @@
+﻿using HLGame.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HLGame.Models
+{
+    public class NumberGenerator_RandomNumbers_DifferFromPrevious : INumberGenerator
+    {
+        public NumberGenerator_RandomNumbers_DifferFromPrevious() { }
+        public int[] Generate(int Count)
+        {
+            int[] returnNumbers = new int[Count];
+
+            int lastNumber = -1;
+            Random rnd = new Random();
+
+            for (int i = 0; i < Count;i++)
+            {
+                int number = rnd.Next(0, 100);
+
+                // number differs add to array
+                if (number != lastNumber)
+                {
+                    lastNumber = number;
+                    returnNumbers[i] = number;
+                }
+                else
+                {
+                    //repeat the same iteration again
+                    i--;
+                }
+            }
+
+            return returnNumbers;
+
+            
+            
+        }
+    }
+}
