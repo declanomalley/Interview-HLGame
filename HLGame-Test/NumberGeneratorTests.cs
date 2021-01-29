4833a30432af57096d239384b78f0ff420522d4b
Declan O'Malley <nalced@hotmail.co.uk>
2021-01-29 13:16:22 +0000

    tdd for testing random number generation.

---
+++ HLGame-Test/NumberGeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿using HLGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HLGame_Test
+{
+    public class NumberGenerator__RandomNumbers_DifferFromPrevious_Tests
+    {
+        [Fact]
+        public void Has_Enough_Numbers()
+        {
+            var generator = new NumberGenerator_RandomNumbers_DifferFromPrevious();
+            var numbers = generator.Generate(10);
+
+            Assert.Equal(10,numbers.Length);
+        }
+
+       
+    }
+}
