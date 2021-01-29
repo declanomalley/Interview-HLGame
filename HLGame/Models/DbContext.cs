490440d3ecc836e5b78332e39ebd03827f45e294
Declan O'Malley <nalced@hotmail.co.uk>
2021-01-29 15:23:40 +0000

    create dbcontext model.

---
+++ HLGame/Models/DbContext.cs
@@ -0,0 +1,31 @@
+﻿using HLGame.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HLGame.Models
+{
+    public class DbContext : IDBContext
+    {
+        public IGame GetGame(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IHighScore[] GetHighScores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveHighScore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
