583076e7abd1ed47943b2fa8e3ca1cd6c6aeefc5
Declan O'Malley <nalced@hotmail.co.uk>
2021-01-29 13:36:46 +0000

    a board to display the numbers tests

---
+++ HLGame-Test/HiLoNumberBoardTests.cs
@@ -0,0 +1,27 @@
+﻿using HLGame.Interface;
+using HLGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HLGame_Test
+{
+    public class HiLoNumberBoardTests
+    {
+        INumberGenerator generator;
+        private const int NumberOfRounds = 10;
+
+        
+
+        [Fact]
+        public void HiLoNumberBoard_Has_Numbers_Generated()
+        {
+            var board = new HiLoNumbersBoard(generator);
+
+            Assert.True(board.Numbers.Length > 0);
+        }
+
+       
+    }
+}
