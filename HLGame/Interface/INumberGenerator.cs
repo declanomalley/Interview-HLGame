3e0b067f7350bd990580becb23a9f7fcc2099ac7
Declan O'Malley <nalced@hotmail.co.uk>
2021-01-29 13:18:20 +0000

    number generator interface.

---
+++ HLGame/Interface/INumberGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HLGame.Interface
+{
+    public interface INumberGenerator
+    {
+        public int[] Generate(int Count);
+    }
+}
