7beb19237aef878fbdaaea5768bb8616639fd72d
Declan O'Malley <nalced@hotmail.co.uk>
2021-01-29 14:55:36 +0000

    define crud contract.

---
+++ HLGame/Interface/ICRUD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HLGame.Interface
+{
+    public interface ICRUD : IGet, IUpdate, Insert, IDelete
+    {
+
+    }
+
+    public interface IGet
+    {
+        public void Get();
+    }
+
+    public interface IUpdate
+    {
+        public bool Update();
+    }
+
+    public interface Insert
+    {
+        public bool Insert();
+    }
+
+    public interface IDelete
+    {
+        public bool Delete();
+    }
+}
